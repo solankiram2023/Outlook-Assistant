081d50257e8277239be2a267aecb30a9ba04fe16
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-12-10 02:36:05 -0500

    Fixed formatting

--- airflow_pipeline/dags/auth/accessToken.py
+++ airflow_pipeline/dags/auth/accessToken.py
@@ -31,19 +31,19 @@ def format_token_response(logger, token_response):
     id_token_claims = message.get("id_token_claims", {})
 
     formatted_token_response = {
-        "id": id_token_claims.get("oid"),
-        "tenant_id": id_token_claims.get("tid"),
-        "name": id_token_claims.get("name"),
-        "email": id_token_claims.get("preferred_username"),
-        "token_type": message.get("token_type"),
-        "access_token": message.get("access_token"),
-        "refresh_token": message.get("refresh_token"),
-        "id_token": message.get("id_token"),
-        "scope": message.get("scope"),
-        "token_source": message.get("token_source"),
-        "iat": datetime.utcfromtimestamp(id_token_claims.get("iat", 0)),
-        "exp": datetime.utcfromtimestamp(id_token_claims.get("exp", 0)),
-        "nonce": id_token_claims.get("aio"),
+        "id"            : id_token_claims.get("oid"),
+        "tenant_id"     : id_token_claims.get("tid"),
+        "name"          : id_token_claims.get("name"),
+        "email"         : id_token_claims.get("preferred_username"),
+        "token_type"    : message.get("token_type"),
+        "access_token"  : message.get("access_token"),
+        "refresh_token" : message.get("refresh_token"),
+        "id_token"      : message.get("id_token"),
+        "scope"         : message.get("scope"),
+        "token_source"  : message.get("token_source"),
+        "iat"           : datetime.utcfromtimestamp(id_token_claims.get("iat", 0)),
+        "exp"           : datetime.utcfromtimestamp(id_token_claims.get("exp", 0)),
+        "nonce"         : id_token_claims.get("aio"),
     }
 
     logger.info("Airflow - auth/accessToken.py - format_token_response() - Formatted the response successfully")
